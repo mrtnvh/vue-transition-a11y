8b099993885d951e0bc9483ff6baa2bb94757119
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2019-11-28 22:08:33 +0100

    build(Pika): Replace Rollup with Pika

--- rollup.shared.js
+++
@@ -1,9 +0,0 @@
-import pkg from './package.json';
-
-export default {
-  input: 'src/index.ts',
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-};
